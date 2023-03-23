9fefbaae93407a4e001e2d9a0fb1b03b61b8b123
ImWez <124311528+ImWezzz@users.noreply.github.com>
2023-03-23 00:49:43 -0300

    Create ping.js

---
+++ commands/ping.js
@@ -0,0 +1,13 @@
+const { HybridBuilder, Context } = require('erine');
+
+const body = {
+    data: new HybridBuilder()
+    .setName("ping")
+    .setAliases("latency")
+    .setDescription("Muestra mi ping"),
+    async code(ctx) {
+        ctx.send(`Mi ping es de: ${ctx.bot.ws.ping}ms`)
+    }
+}
+
+module.exports = { body }
