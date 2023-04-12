90708730509a4036dac5a36119fda6615f6d2e35
ImWez <124311528+ImWezzz@users.noreply.github.com>
2023-04-11 22:37:08 -0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,9 +3,9 @@ const event = {
     name: "ready",
     async code(client) {
         client.skyfold.sync(client.token, client.user.id).then(() => {
-            console.log("Slash commands cargados exitosamente!")
+            console.log("Slash commands uploaded to discord API!")
         })
-        console.log(`Me conecte en: ${client.user.username}`)
+        console.log(`Loggin in: ${client.user.username}`)
     }
 } 
 
